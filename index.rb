4d99fa913879bea35a8a23e939ac78361f7040cc
Ricky Chon <rickychon99@gmail.com>
2020-07-30 12:08:26 -0400

    Set default prefix

--- index.rb
+++ index.rb
@@ -3,17 +3,19 @@
 require 'discordrb'
 require 'dotenv'
 
+default_prefix = ENV['PREFIX'] || '!'
+
 Dotenv.load
 bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
 
 puts "This bot's invite URL is #{bot.invite_url}."
 puts 'Click on it to invite it to your server.'
 
-bot.message(with_text: "#{ENV['PREFIX']}ping") do |event|
+bot.message(with_text: "#{default_prefix}ping") do |event|
   event.respond 'Pong!'
 end
 
-bot.message(with_text: "#{ENV['PREFIX']}game") do |event|
+bot.message(with_text: "#{default_prefix}game") do |event|
   event.respond "You are playing #{event.author.game || 'nothing at all'}"
 end
 
